91b810ffa4934138b559f87645511d69c9021dd4
DuodecimAD <antony.dekens@gmail.com>
2022-12-16 20:28:42 +0000

    Fri, Dec 16, 2022, 8:28 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -12,7 +12,7 @@ int main(void)
         // get height
         height = get_int("Height :\n");
 
-        // check if between 1 and 8
+        // check if not between 1 and 8
         while(height < 1 || height > 8){
             height = get_int("Height :\n");
         }
@@ -20,7 +20,7 @@ int main(void)
         // go mario, go, jump above this and go deliver Peach
         for(int i = 1; i <= height; i++){
             for(int s = height-i; s > 0; s--){
-                printf(".");
+                printf(" ");
             }
             for(int j = 1; j <= i; j++){
                 printf("#");
