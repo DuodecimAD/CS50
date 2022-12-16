d58b08d56ac5702ef9e4d021e11433a1d4c93b70
DuodecimAD <antony.dekens@gmail.com>
2022-12-16 20:35:30 +0000

    Fri, Dec 16, 2022, 8:35 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,32 +5,30 @@
 // Never did any C by the way, i should go watch the 3h video of week 1 now that i'm done
 // It was fun, i wish it was an accomplishement but it's probably a basic exercise ...
 
+int height;
+
 int main(void)
 {
-    int height;
+    // get height
+    height = get_int("Height :");
 
-        // get height
-        height = get_int("Height :\n");
+    // check if not between 1 and 8
+    while(height < 1 || height > 8){
+        height = get_int("Height :");
+    }
 
-        // check if not between 1 and 8
-        while(height < 1 || height > 8){
-            height = get_int("Height :\n");
+    // go mario, go, jump above this and go deliver Peach
+    for(int i = 1; i <= height; i++){
+        for(int s = height-i; s > 0; s--){
+            printf(" ");
         }
-
-        // go mario, go, jump above this and go deliver Peach
-        for(int i = 1; i <= height; i++){
-            for(int s = height-i; s > 0; s--){
-                printf(" ");
-            }
-            for(int j = 1; j <= i; j++){
-                printf("#");
-            }
-            printf("  ");
-            for(int e = 1; e <= i; e++){
-                printf("#");
-            }
-            printf("\n");
+        for(int j = 1; j <= i; j++){
+            printf("#");
+        }
+        printf("  ");
+        for(int e = 1; e <= i; e++){
+            printf("#");
         }
         printf("\n");
-
+    }
 }
