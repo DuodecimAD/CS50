7135ec1271944f893b74786825589af53e836d0e
DuodecimAD <antony.dekens@gmail.com>
2022-12-16 19:43:21 +0000

    Fri, Dec 16, 2022, 7:43 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,11 +9,11 @@ int main(void)
         height = get_int("Height :\n");
 
         for(int i = 1; i <= height; i++){
+            for(int s = height-1; s > 0; s--){
+                printf(" ");
+            }
             for(int j = 1; j <= i; j++){
-                 for(int s = height-1; s > 0; s--){
-                    printf(" ");
-                 }
-                 printf("#");
+                printf("#");
             }
             printf("  ");
             for(int e = 1; e <= i; e++){
