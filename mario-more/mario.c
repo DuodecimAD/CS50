5b2551e615ee99336bcaeb3679e2fac94045035b
DuodecimAD <antony.dekens@gmail.com>
2022-12-16 20:19:50 +0000

    Fri, Dec 16, 2022, 8:19 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -21,7 +21,7 @@ int main(void)
         // go mario, go, jump above this and go deliver Peach
         for(int i = 1; i <= height; i++){
             for(int s = height-i; s > 0; s--){
-                printf(" ");
+                printf(".");
             }
             for(int j = 1; j <= i; j++){
                 printf("#");
