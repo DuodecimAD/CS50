b070b90311589c94a127ed0dcbc25377dbf34e86
DuodecimAD <antony.dekens@gmail.com>
2022-12-17 12:12:53 +0000

    Sat, Dec 17, 2022, 12:12 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,16 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-long input;
-long copy;
-long card;
-int length = 0;
-int mult = 0;
-int digit1 = 0;
-int digit2 = 0;
-int sum1 = 0;
-int sum2 = 0;
-int checksum = 0;
+long input, copy, card;
+int length = 0, mult = 0, digit1 = 0, digit2 = 0, sum1 = 0, sum2 = 0, checksum = 0;
 
 int main(void)
 {
@@ -19,20 +11,21 @@ int main(void)
     card = input;
 
     // get length of card number
-    while(copy!=0)
+    while (copy != 0)
     {
-       copy=copy/10;
-       length++;
+        copy = copy / 10;
+        length++;
     }
 
     // get which card it is
-    for(int z = 0; z < length-2; z++){
+    for (int z = 0; z < length - 2; z++)
+    {
         card = card / 10;
     }
 
 
     // calculate checksum
-    for (int i = 0; i <= length; i+=2)
+    for (int i = 0; i <= length; i += 2)
     {
         digit2 = input % 10;
         input = input / 10;
@@ -42,34 +35,35 @@ int main(void)
         input = input / 10;
         mult = digit1 * 2;
 
-        if(mult<10){
+        if (mult < 10)
+        {
             sum1 = sum1 + mult;
-        }else{
+        }
+        else
+        {
             sum1 = sum1 + (mult % 10);
             mult = mult / 10;
             sum1 = sum1 + mult;
         }
-
         checksum = sum1 + sum2;
-
     }
 
-    // printf("somme is : %d\n", checksum);
-
-    if(checksum % 10 == 0)
+    // final check first digits card, card length and checksum
+    if (checksum % 10 == 0)
     {
-        if((card == 34 || card == 37) && length == 15)
+        if ((card == 34 || card == 37) && length == 15)
         {
             printf("AMEX\n");
         }
-        else if((card > 50 && card < 56) && length == 16)
+        else if ((card > 50 && card < 56) && length == 16)
         {
             printf("MASTERCARD\n");
         }
         else
         {
             card =  card / 10;
-            if(card == 4 && (length == 13 || length == 16)){
+            if (card == 4 && (length == 13 || length == 16))
+            {
                 printf("VISA\n");
             }
             else
