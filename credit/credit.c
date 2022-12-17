f42e223fc3cbfc9c485cc8238c925dc861f86bec
DuodecimAD <antony.dekens@gmail.com>
2022-12-17 08:01:50 +0000

    Sat, Dec 17, 2022, 8:01 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int number;
+int mult = 0;
 int sum1 = 0;
 
 int main(void)
@@ -10,12 +11,19 @@ int main(void)
 
     for (int i = number.length-1; i >= 0; i-=2)
     {
+
+    }
+
+    for (int j = number.length-1; j >= 0; j-=2)
+    {
         if(i<10){
             sum1 = sum + i;
         }else{
             i
         }
 
-        printf(sum1);
+
     }
+    printf(sum1);
+
 }
