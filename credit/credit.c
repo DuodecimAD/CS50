fec518d0685294995b8be5d9bc82f574498b9c4c
DuodecimAD <antony.dekens@gmail.com>
2022-12-17 09:09:29 +0000

    Sat, Dec 17, 2022, 9:09 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 long input;
+long a_number[];
 int length = 0;
 int mult = 0;
 int digit = 0;
@@ -18,10 +19,10 @@ int main(void)
        input=input/10;
        length++;
     }
-    long a_number[length] = input;
+    a_number[length] = input;
 
 
-    printf("card array is : %d\n", a_number[]);
+    printf("card array is : %d\n", a_number);
 
     for (int i = length-1; i >= 0; i-=2)
     {
