b527ad73c871bc22712260b6ca1b49c4560f06bc
DuodecimAD <antony.dekens@gmail.com>
2022-12-17 10:02:27 +0000

    Sat, Dec 17, 2022, 10:02 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,15 +5,14 @@ long input;
 
 int length = 0;
 int mult = 0;
-int digit = 0;
+int digit1 = 0;
+int digit2 = 0;
 int sum1 = 0;
 int sum2 = 0;
 
 int main(void)
 {
     input = get_long("credit card number :");
-  printf("card  is : %ld\n", input);
-
 
     // get length of card number
     while(input!=0)
@@ -22,21 +21,18 @@ int main(void)
        length++;
     }
 
-
-    printf("length is : %d\n", length);
-
     for (int i = length-1; i >= 0; i-=2)
     {
-        sum2 = sum2 + (input % 10);
+        printf("input is : %ld\n", input);
+        digit2 = input % 10;
         input = input / 10;
+        sum2 = sum2 + digit2;
+        printf("input is : %ld\n", input);
 
-        digit = input % 10;
+        digit1 = input % 10;
         input = input / 10;
-        //printf("digit is : %d\n", digit);
-
-
-        mult = digit * 2;
-
+        mult = digit1 * 2;
+        printf("input is : %ld\n", input);
         printf("sum2 is : %d\n", sum2);
 
         if(mult<10){
@@ -49,5 +45,6 @@ int main(void)
 
     }
     printf("somme is : %d\n",sum1);
+    printf("somme2 is : %d\n",sum2);
 
 }
