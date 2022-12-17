97c8dde04910e9ad67d5d3228ad64c0e07ef01c1
DuodecimAD <antony.dekens@gmail.com>
2022-12-17 07:57:04 +0000

    Sat, Dec 17, 2022, 7:57 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int number;
+int sum1 = 0;
+
+int main(void)
 {
-    
+    number = get_int("credit card number :");
+
+    for (int i = number.length-1; i > 0; i-2)
+    {
+        if(i<10){
+            sum1 = sum + i;
+        }else{
+            
+        }
+
+        printf(sum1);
+    }
 }
