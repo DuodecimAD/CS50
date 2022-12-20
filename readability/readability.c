17640a13d280a31417e9423bf05d08b6391d8d71
DuodecimAD <antony.dekens@gmail.com>
2022-12-20 13:51:47 +0000

    Tue, Dec 20, 2022, 1:51 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,18 +3,23 @@
 #include <string.h>
 #include <ctype.h>
 
-string input;
+
 
 int count_letters(string text);
+int count_words(string text);
 
 
 int main(void)
 {
-    input = get_string("Text: ");
+    string input = get_string("Text: ");
     printf("Text: %s\n", input);
 
     int letters = count_letters(input);
     printf("letters : %i\n", letters);
+
+    int words = count_words(input);
+    printf("words : %i\n", letters);
+
 
 }
 
@@ -25,6 +30,21 @@ int count_letters(string text)
     for (int i = 0; i < strlen(text); i++)
     {
         if(isalpha(text[i]))
+        {
+            j++;
+        }
+
+    }
+    return j;
+}
+
+int count_words(string text)
+{
+    int j = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if(isspace(text[i]))
         {
             j++;
         }
