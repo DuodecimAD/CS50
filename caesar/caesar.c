9f8fd91c4e233a8d6cbc504d73772cf9ab8f582f
DuodecimAD <antony.dekens@gmail.com>
2022-12-20 20:48:38 +0000

    Tue, Dec 20, 2022, 8:48 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main(int argc, string argv[])
+int main(int argc, string argv[1])
 {
-    if(isdigit(argv))
+    int key = argv[1];
+
+    if(isdigit(key))
     {
-        int key = argv;
+        string input = get_string("plaintext: \n");
+
+        for (int i = 0; i < strlen(input); i++)
+        {
+            input[i] = (input[i] + key) % 26;
+        }
+
+        printf("ciphertext: %s\n", input);
+
     }
-    
 
-    string input = get_string("plaintext: \n");
 
-    for (int i = 0; i < strlen(input); i++)
-    {
-        input[i] = (input[i] + key) % 26;
-    }
 
-    printf("ciphertext: %s\n", input);
 }
