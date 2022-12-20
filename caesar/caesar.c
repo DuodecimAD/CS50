4dab08e698998deb082590a7ea79920d6386805f
DuodecimAD <antony.dekens@gmail.com>
2022-12-20 20:39:52 +0000

    Tue, Dec 20, 2022, 8:39 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,15 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
-#inculde <ctype.h>
+
 
 int main(int argc, string argv[])
 {
-    int key = argv[];
+    int key = argv;
 
     string input = get_string("plaintext: \n");
 
     for (int i = 0; i < strlen(input); i++)
     {
-        input[i] = input[i] + key;
+        input[i] = (input[i] + key) % 26;
     }
+
+    printf("ciphertext: %s\n", input);
 }
