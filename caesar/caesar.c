ab680074873719c4de7e0d3473dd42fb9d814d37
DuodecimAD <antony.dekens@gmail.com>
2022-12-20 20:32:43 +0000

    Tue, Dec 20, 2022, 8:32 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#inculde <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -7,5 +8,11 @@ int main(int argc, string argv[])
 
     string input = get_string("plaintext: \n");
 
-    for (int i = )
+    for (int i = 0; i < strlen(input); i++)
+    {
+        if (isalpha(input) && islower(input))
+        {
+            
+        }
+    }
 }
