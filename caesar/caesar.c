a3cc53ae4fd42b7cd86563d0c35c0e957b21fd58
DuodecimAD <antony.dekens@gmail.com>
2022-12-20 21:52:25 +0000

    Tue, Dec 20, 2022, 9:52 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,14 +8,11 @@ bool only_digits(string check);
 
 int main(int argc, string argv[])
 {
-    if(argc != 2){
-
+    if (argc != 2)
+    {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-
-
-
 
     if (!only_digits(argv[1]) || !argv[1])
     {
@@ -24,26 +21,21 @@ int main(int argc, string argv[])
     }
     else
     {
-
         string input = get_string("plaintext: ");
         int key = atoi(argv[1]);
 
         for (int i = 0; i < strlen(input); i++)
         {
-            if(isalpha(input[i]) && islower(input[i]))
+            if (isalpha(input[i]) && islower(input[i]))
             {
-               input[i] = (((input[i] - 97) + key) % 26) + 97;
+                input[i] = (((input[i] - 97) + key) % 26) + 97;
             }
-            else if(isalpha(input[i]) && isupper(input[i]))
+            else if (isalpha(input[i]) && isupper(input[i]))
             {
                 input[i] = (((input[i] - 65) + key) % 26) + 65;
             }
-
-
         }
-
         printf("ciphertext: %s\n", input);
-
     }
 
 }
@@ -59,6 +51,5 @@ bool only_digits(string s)
             return false;
         }
     }
-        return true;
-
+    return true;
 }
