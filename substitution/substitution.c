e821eca0fc2b6b03da9f7a86cff05525eb85e018
DuodecimAD <antony.dekens@gmail.com>
2022-12-21 20:03:08 +0000

    Wed, Dec 21, 2022, 8:03 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,6 +4,12 @@
 int main(int argc, string argv[])
 {
 
+    if (argc != 2)
+    {
+        if(argv[1])
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
 
     printf("%c\n", argv[1][0]);
 
