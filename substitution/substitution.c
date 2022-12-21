42691ec5a191f5baf75fbef266c6e41e385066d5
DuodecimAD <antony.dekens@gmail.com>
2022-12-21 21:02:43 +0000

    Wed, Dec 21, 2022, 9:02 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -9,14 +9,14 @@ int toascii(int c);
 int main(int argc, string argv[])
 {
 
-
+    string length = argv[1];
 
     if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    else if (strlen(argv[1]) != 26)
+    else if (strlen(length) != 26)
     {
         printf("Key must contain 26 characters.\n");
     }
