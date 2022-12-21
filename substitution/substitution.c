eb283ab415c16b00f0c2bfc63a50a0950abd4c4e
DuodecimAD <antony.dekens@gmail.com>
2022-12-21 20:09:10 +0000

    Wed, Dec 21, 2022, 8:09 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -10,13 +10,27 @@ int main(int argc, string argv[])
         return 1;
     }
 
-    
+
 
     string input = get_string("plaintext: ");
 
   //  printf("plaintext: %c", argv[1][0]);
 
 
-    printf("ciphertext: %c", argv[1][0]);
+        int key = atoi(argv[1]);
+
+        for (int i = 0; i < strlen(input); i++)
+        {
+            if (isalpha(input[i]) && islower(input[i]))
+            {
+                input[i] = (((input[i] - 97) + key) % 26) + 97;
+            }
+            else if (isalpha(input[i]) && isupper(input[i]))
+            {
+                input[i] = (((input[i] - 65) + key) % 26) + 65;
+            }
+        }
+        printf("ciphertext: %s\n", input);
+
 
 }
