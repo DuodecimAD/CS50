97f4c70580be86a22d3550778ae743748e2fa269
DuodecimAD <antony.dekens@gmail.com>
2022-12-29 15:36:19 +0000

    Thu, Dec 29, 2022, 3:36 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -77,6 +77,7 @@ int main(int argc, string argv[])
                 printf("Invalid vote.\n");
                 return 4;
             }
+            printf("%i %s\n", i, preferences[i][j] )
         }
 
         printf("\n");
