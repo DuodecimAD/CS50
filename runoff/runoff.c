7d4e6553462d4ca6e7cf7bf5df5b422821969e0a
DuodecimAD <antony.dekens@gmail.com>
2022-12-29 15:31:28 +0000

    Thu, Dec 29, 2022, 3:31 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,7 +128,7 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    preferences[voter][rank];
+    preferences[voter][rank] = name;
     return false;
 }
 
