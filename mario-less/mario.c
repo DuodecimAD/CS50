5f484fe756d4ab18ec88454bb5058281d6829a5d
DuodecimAD <antony.dekens@gmail.com>
2022-12-31 12:14:44 +0000

    Sat, Dec 31, 2022, 12:14 PM +00:00

---
+++ mario-less/mario.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int floors = get_int("Height : ");
+
+    for (int i = 0; i < floors; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
